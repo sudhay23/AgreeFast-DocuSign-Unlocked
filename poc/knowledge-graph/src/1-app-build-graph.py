97d7a4591d88149164f0b13ba988f8cbc8ab71b1
sudhay23 <sudhay2001@gmail.com>
2025-01-03 20:34:16 +0530

    Added CypherQA and Vector Search scripts

--- poc/knowledge-graph/src/1-app-build-graph.py
+++ poc/knowledge-graph/src/1-app-build-graph.py
@@ -9,19 +9,17 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 llm = provision_chat_model()
 graph = Neo4jGraph(url=os.getenv("NEO4J_URI"),database=f"neo4j",username=os.getenv("NEO4J_USERNAME"),password=os.getenv("NEO4J_PASSWORD"))
-document_link = "http://127.0.0.1:5500/static_store/Employee%20Seperation.pdf"
+# document_link = "http://127.0.0.1:5500/static_store/Employee%20Seperation.pdf"
+document_link = "http://127.0.0.1:5500/static_store/MSA.pdf"
 splitter = RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=100)
 loader = OnlinePDFLoader(document_link)
 
 llm_transformer = LLMGraphTransformer(llm=llm)
 
 documents = loader.load()
-documents = splitter.split_documents(documents)
-# print(documents)
-# print("\n\n\n")
 
+documents = splitter.split_documents(documents)
 
 graph_documents = llm_transformer.convert_to_graph_documents(documents)
-# print(graph_documents,"\n\n\n\n")
 
 graph.add_graph_documents(graph_documents,baseEntityLabel=True,include_source=True)
---
+++ poc/knowledge-graph/src/3-app-graph-vector-search.py
@@ -0,0 +1,34 @@
+import dotenv
+dotenv.load_dotenv("./.env")
+import os,time
+from utils.model import provision_chat_model
+from langchain_neo4j import Neo4jVector
+from langchain.embeddings.ollama import OllamaEmbeddings
+
+llm = provision_chat_model()
+embedding_model = OllamaEmbeddings(base_url=os.getenv("OLLAMA_URI"),model="nomic-embed-text")
+# graph = Neo4jGraph(url=os.getenv("NEO4J_URI"),database=f"neo4j",username=os.getenv("NEO4J_USERNAME"),password=os.getenv("NEO4J_PASSWORD"))
+
+
+neo4j_vector = Neo4jVector(embedding_model,url=os.getenv("NEO4J_URI"),database=f"neo4j",username=os.getenv("NEO4J_USERNAME"),password=os.getenv("NEO4J_PASSWORD"))
+vector_store = neo4j_vector.from_existing_graph(embedding=embedding_model,search_type="vector",node_label="Document",text_node_properties=['text'],embedding_node_property="vector_embedding",)
+
+
+# query = "should i return my properties"
+# query = "who is jane doe"
+# query = "What are the dates i should be bothered by"
+# query = "What are the dates i should be bothered by and what happened in each day"
+# query = "who is the HR"
+# query = "who should i contact about legal issues and tell in detail"
+# query = "how much was the person paid while leaving"
+# query = "will they work on holidays"
+query = "what are the renewal terms"
+
+docs_with_score = vector_store.similarity_search_with_score(query, k=9)
+
+for doc, score in docs_with_score:
+    print("-" * 80)
+    print("Score: ", score)
+    print(doc.page_content)
+    print("-" * 80)
+
