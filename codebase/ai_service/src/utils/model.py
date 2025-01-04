b9f58f6c620802de337652e5af0dd489efd14ddd
sudhay23 <sudhay2001@gmail.com>
2025-01-04 22:38:17 +0530

    Chat endpoint complete

--- codebase/ai_service/src/utils/model.py
+++ codebase/ai_service/src/utils/model.py
@@ -1,6 +1,7 @@
 import sys, os
 sys.path.append(os.path.abspath(os.path.join('..')))
 from langchain_openai import AzureChatOpenAI
+from langchain_ollama import OllamaEmbeddings
 
 # Azure GPT-4o
 def provision_chat_model():
@@ -11,3 +12,7 @@ def provision_chat_model():
                                 api_key=os.getenv("AZURE_OPENAI_API_KEY"),
                             )
     return chat_model
+
+# Ollama - nomic-embed-text
+def provision_embedding_model():
+    return OllamaEmbeddings(base_url=os.getenv("OLLAMA_URI"),model="nomic-embed-text")
