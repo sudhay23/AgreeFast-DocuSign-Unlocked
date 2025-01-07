9a6544d08b11dbe1de5e2b83440e41d434141bb7
Mritul Senthilkumar <mritul2003@gmail.com>
2025-01-07 16:19:32 +0530

    feat: Mail redesign complete

--- codebase/frontend/src/components/ActivationWarning/index.tsx
+++ codebase/frontend/src/components/ActivationWarning/index.tsx
@@ -9,7 +9,7 @@ export const ActivationWarning = () => {
         <MailOpen01Icon className="animate-jiggle" size={100} />
         <div>
           <h2 className="text-2xl font-bold">
-            This envelope has not been activated yet
+            The agreefast dashboard for this envelope has not been activated yet
           </h2>
           <p className="text-gray-300">
             Reach out to the envelope sender/owner regarding the activation
