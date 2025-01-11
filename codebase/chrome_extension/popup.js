56664eeeb0e06890d0c647de6adc61f0eb200de1
sudhay23 <sudhay2001@gmail.com>
2025-01-11 12:14:52 +0530

    Added feature to launch agreefast dashboard from the Chrome Extension

--- codebase/chrome_extension/popup.js
+++ codebase/chrome_extension/popup.js
@@ -4,13 +4,10 @@ const sleep = async (ms) => {
   });
 };
 
+// Connect Agreefast to Docusign button
 const setupAgreefastDocusignConnectBtn = document.querySelector(
   "#setup_agreefast_docusign_connect_btn"
 );
-
-const link = document.querySelector("a");
-
-link.href = "https://google.com";
 
 setupAgreefastDocusignConnectBtn.addEventListener("click", async (e) => {
   //  Open "https://admindemo.docusign.com/connect" / "https://apps-d.docusign.com/admin/connect"
@@ -21,5 +18,19 @@ setupAgreefastDocusignConnectBtn.addEventListener("click", async (e) => {
 
   await chrome.runtime.sendMessage({
     message: "start_connect_job",
+  });
+});
+
+// Launch Agreefast dashboard (if available)
+const lauchAgreefastDashboardBtn = document.querySelector(
+  "#launch_agreefast_dashboard_btn"
+);
+
+lauchAgreefastDashboardBtn.addEventListener("click", async (e) => {
+  // Get current tab in view and send the targeted message
+  chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
+    await chrome.tabs.sendMessage(tabs[0].id, {
+      message: "launch_agreefast_dashboard",
+    });
   });
 });
