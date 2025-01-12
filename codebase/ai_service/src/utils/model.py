0138be709e502771744f161bbade277f9b5e5d4d
sudhay23 <sudhay2001@gmail.com>
2025-01-12 23:33:03 +0530

    Use GPT 4o-mini for ICS generation

--- codebase/ai_service/src/utils/model.py
+++ codebase/ai_service/src/utils/model.py
@@ -4,10 +4,10 @@ from langchain_openai import AzureChatOpenAI
 from langchain_ollama import OllamaEmbeddings
 
 # Azure GPT-4o
-def provision_chat_model():
+def provision_chat_model(azure_deployment_name=os.getenv("AZURE_OPENAI_DEPLOYMENT_1")):
     chat_model = AzureChatOpenAI(
                                 azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
-                                azure_deployment=os.getenv("AZURE_OPENAI_DEPLOYMENT"),
+                                azure_deployment=azure_deployment_name,
                                 api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
                                 api_key=os.getenv("AZURE_OPENAI_API_KEY"),
                             )
